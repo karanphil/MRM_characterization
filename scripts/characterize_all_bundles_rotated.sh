c151525a8d33083f7b85c7de46e7aac6818966af
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-12-02 17:34:03 -0500

    WIP

---
+++ scripts/characterize_all_bundles_rotated.sh
@@ -0,0 +1,18 @@
+wdir="/home/pkaran/Samsung/data/MT_Diffusion/rotated_myelo_inferno";
+# wdir="/home/local/USHERBROOKE/karp2601/Samsung/data/MT_Diffusion/myelo_inferno";
+source_dir="source";
+# source_dir="Research/source";
+cd $wdir;
+bin_width=5;
+bin_width_dir="${bin_width}_degree_bins";
+poly_order=8;
+subject="sub-001_ses-1";
+for bundle in rbx_flow/results_rbx/${subject}/Masks/*;
+    do bundle_name=$(basename -- "${bundle%%.*}");
+    if [ 1 == 1 ];
+    #if [ "$bundle_name" = "AF_L" ];
+        then echo $bundle_name;
+        mkdir -p "orientation_analysis/${subject}/characterization_per_bundle/${bin_width_dir}/${bundle_name}";
+        python ~/${source_dir}/MRM_characterization/scripts/scil_characterize_orientation_dependence.py new_peaks/${subject}/peaks.nii.gz  new_peaks/${subject}/peak_values.nii.gz tractoflow/output/results/${subject}/DTI_Metrics/${subject}__fa.nii.gz new_peaks/${subject}/nufo.nii.gz tractoflow/output/results/${subject}/Segment_Tissues/${subject}__mask_wm.nii.gz orientation_analysis/${subject}/characterization_per_bundle/${bin_width_dir}/${bundle_name} --measures ihmt_flow/output/results/${subject}/Compute_ihMT/Register_ihMT_maps/${subject}__MTR_warped.nii.gz ihmt_flow/output/results/${subject}/Compute_ihMT/Register_ihMT_maps/${subject}__ihMTR_warped.nii.gz ihmt_flow/output/results/${subject}/Compute_ihMT/Register_ihMT_maps/${subject}__MTsat_warped.nii.gz ihmt_flow/output/results/${subject}/Compute_ihMT/Register_ihMT_maps/${subject}__ihMTsat_warped.nii.gz --in_e1 tractoflow/output/results/${subject}/DTI_Metrics/${subject}_evecs_v1.nii.gz --measures_names MTR ihMTR MTsat ihMTsat --save_npz_files --in_roi $bundle --bin_width_1f $bin_width --poly_order $poly_order --use_weighted_polyfit --scale_poly_order --min_nb_voxels 1;
+    fi;
+done;
--- scripts/scil_characterize_orientation_dependence.py
+++ scripts/scil_characterize_orientation_dependence.py
@@ -255,92 +255,92 @@ def main():
     # Note: Beyond this point, no measures with nb_voxels below min_nb_voxels
     # will be saved. These bins will have None as measures value.
     # Note: all the data is now saved, but the plots are not adapted.
-    print("Computing two-fiber means.")
-    bins, measure_means, nb_voxels, labels =\
-        compute_two_fibers_means(peaks, peak_values,
-                                        wm_mask, affine,
-                                        nufo, measures, roi=roi,
-                                        bin_width=args.bin_width_2f)
+    # print("Computing two-fiber means.")
+    # bins, measure_means, nb_voxels, labels =\
+    #     compute_two_fibers_means(peaks, peak_values,
+    #                                     wm_mask, affine,
+    #                                     nufo, measures, roi=roi,
+    #                                     bin_width=args.bin_width_2f)
     
-    measure_means_diag = np.diagonal(measure_means, axis1=1, axis2=2)
-    measure_means_diag = np.swapaxes(measure_means_diag, 1, 2)
-    nb_voxels_diag = np.diagonal(nb_voxels, axis1=1, axis2=2)
+    # measure_means_diag = np.diagonal(measure_means, axis1=1, axis2=2)
+    # measure_means_diag = np.swapaxes(measure_means_diag, 1, 2)
+    # nb_voxels_diag = np.diagonal(nb_voxels, axis1=1, axis2=2)
 
-    print("Analysing two-fiber delta_m_max.")
-    slope, origin, delta_m_max, frac_thrs_mid, min_bins, max_bins =\
-        analyse_delta_m_max(bins, measure_means_diag[..., :nb_measures],
-                            sf_delta_m_max, nb_voxels_diag)
-    print("Analysis found these bins as minima and maxima: ")
-    for i in range(nb_measures):
-        print(str(measures_name[i]) + " minimum at " + str(min_bins[i]) + " degrees")
-        print(str(measures_name[i]) + " maximum at " + str(max_bins[i]) + " degrees")
+    # print("Analysing two-fiber delta_m_max.")
+    # slope, origin, delta_m_max, frac_thrs_mid, min_bins, max_bins =\
+    #     analyse_delta_m_max(bins, measure_means_diag[..., :nb_measures],
+    #                         sf_delta_m_max, nb_voxels_diag)
+    # print("Analysis found these bins as minima and maxima: ")
+    # for i in range(nb_measures):
+    #     print(str(measures_name[i]) + " minimum at " + str(min_bins[i]) + " degrees")
+    #     print(str(measures_name[i]) + " maximum at " + str(max_bins[i]) + " degrees")
 
-    print("Saving delta_m_max fit results.")
-    for i in range(slope.shape[-1]):
-        out_path = out_folder / str(str(measures_name[i]) + "_delta_m_max_fit.npy")
-        np.save(out_path, np.concatenate(([origin[i]], [slope[i]])))
+    # print("Saving delta_m_max fit results.")
+    # for i in range(slope.shape[-1]):
+    #     out_path = out_folder / str(str(measures_name[i]) + "_delta_m_max_fit.npy")
+    #     np.save(out_path, np.concatenate(([origin[i]], [slope[i]])))
 
-    if args.save_npz_files:
-        print("Saving results as npz files.")
-        out_path = npz_folder / "2f_original_results"
-        save_results_as_npz(bins, measure_means[..., :nb_measures], nb_voxels,
-                            measures_name, out_path)
-        if correction:
-            out_path = npz_folder / '2f_corrected_results'
-            save_results_as_npz(bins, measure_means[..., nb_measures:],
-                                nb_voxels, measures_name, out_path)
+    # if args.save_npz_files:
+    #     print("Saving results as npz files.")
+    #     out_path = npz_folder / "2f_original_results"
+    #     save_results_as_npz(bins, measure_means[..., :nb_measures], nb_voxels,
+    #                         measures_name, out_path)
+    #     if correction:
+    #         out_path = npz_folder / '2f_corrected_results'
+    #         save_results_as_npz(bins, measure_means[..., nb_measures:],
+    #                             nb_voxels, measures_name, out_path)
 
-    if args.save_plots:
-        print("Saving two-fiber results as plots.")
-        plot_3d_means(bins, measure_means[0, :, :, :nb_measures], plots_folder,
-                      measures_name, nametype="original")
-        plot_multiple_means(bins, measure_means_diag[..., :nb_measures],
-                            nb_voxels_diag, plots_folder, measures_name,
-                            labels=labels, legend_title=r"Peak$_1$ fraction",
-                            endname="2D_2f", delta_max=delta_m_max,
-                            delta_max_slope=slope, delta_max_origin=origin,
-                            p_frac=frac_thrs_mid, nametype="original")
-        if correction:
-            plot_3d_means(bins, measure_means[0, :, :, nb_measures:],
-                          plots_folder, measures_name, nametype="corrected")
-            plot_multiple_means(bins, measure_means_diag[..., nb_measures:],
-                                nb_voxels_diag, plots_folder, measures_name,
-                                labels=labels,
-                                legend_title=r"Peak$_1$ fraction",
-                                endname="2D_2f", markers='s',
-                                nametype="corrected")
+    # if args.save_plots:
+    #     print("Saving two-fiber results as plots.")
+    #     plot_3d_means(bins, measure_means[0, :, :, :nb_measures], plots_folder,
+    #                   measures_name, nametype="original")
+    #     plot_multiple_means(bins, measure_means_diag[..., :nb_measures],
+    #                         nb_voxels_diag, plots_folder, measures_name,
+    #                         labels=labels, legend_title=r"Peak$_1$ fraction",
+    #                         endname="2D_2f", delta_max=delta_m_max,
+    #                         delta_max_slope=slope, delta_max_origin=origin,
+    #                         p_frac=frac_thrs_mid, nametype="original")
+    #     if correction:
+    #         plot_3d_means(bins, measure_means[0, :, :, nb_measures:],
+    #                       plots_folder, measures_name, nametype="corrected")
+    #         plot_multiple_means(bins, measure_means_diag[..., nb_measures:],
+    #                             nb_voxels_diag, plots_folder, measures_name,
+    #                             labels=labels,
+    #                             legend_title=r"Peak$_1$ fraction",
+    #                             endname="2D_2f", markers='s',
+    #                             nametype="corrected")
 
-    if args.compute_three_fiber_crossings:
-        print("Computing 3 crossing fibers means.")
-        bins, measure_means, nb_voxels, labels =\
-            compute_three_fibers_means(peaks, peak_values, wm_mask, affine,
-                                    nufo, measures, bin_width=args.bin_width_3f,
-                                    roi=roi)
+    # if args.compute_three_fiber_crossings:
+    #     print("Computing 3 crossing fibers means.")
+    #     bins, measure_means, nb_voxels, labels =\
+    #         compute_three_fibers_means(peaks, peak_values, wm_mask, affine,
+    #                                 nufo, measures, bin_width=args.bin_width_3f,
+    #                                 roi=roi)
         
-        if args.save_npz_files:
-            print("Saving results as npz files.")
-            out_path = npz_folder / "3f_original_results"
-            save_results_as_npz(bins, measure_means[..., :nb_measures],
-                                nb_voxels, measures_name, out_path)
-            if correction:
-                out_path = npz_folder / '3f_corrected_results'
-                save_results_as_npz(bins, measure_means[..., nb_measures:],
-                                    nb_voxels, measures_name, out_path)
+    #     if args.save_npz_files:
+    #         print("Saving results as npz files.")
+    #         out_path = npz_folder / "3f_original_results"
+    #         save_results_as_npz(bins, measure_means[..., :nb_measures],
+    #                             nb_voxels, measures_name, out_path)
+    #         if correction:
+    #             out_path = npz_folder / '3f_corrected_results'
+    #             save_results_as_npz(bins, measure_means[..., nb_measures:],
+    #                                 nb_voxels, measures_name, out_path)
 
-        if args.save_plots:
-            print("Saving three-fiber results as plots.")
-            plot_multiple_means(bins, measure_means[..., :nb_measures],
-                                nb_voxels, plots_folder,
-                                measures_name, endname="2D_3f", labels=labels,
-                                legend_title=r"Peak$_1$ fraction",
-                                nametype="original")
-            if correction:
-                plot_multiple_means(bins, measure_means[..., nb_measures:],
-                                    nb_voxels, plots_folder,
-                                    measures_name, endname="2D_3f",
-                                    labels=labels,
-                                    legend_title=r"Peak$_1$ fraction",
-                                    nametype="corrected", markers='s')
+    #     if args.save_plots:
+    #         print("Saving three-fiber results as plots.")
+    #         plot_multiple_means(bins, measure_means[..., :nb_measures],
+    #                             nb_voxels, plots_folder,
+    #                             measures_name, endname="2D_3f", labels=labels,
+    #                             legend_title=r"Peak$_1$ fraction",
+    #                             nametype="original")
+    #         if correction:
+    #             plot_multiple_means(bins, measure_means[..., nb_measures:],
+    #                                 nb_voxels, plots_folder,
+    #                                 measures_name, endname="2D_3f",
+    #                                 labels=labels,
+    #                                 legend_title=r"Peak$_1$ fraction",
+    #                                 nametype="corrected", markers='s')
 
 if __name__ == "__main__":
     main()
